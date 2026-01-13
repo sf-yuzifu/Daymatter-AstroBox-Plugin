55fef8124af4c112831c20c643c59d399e3200c7
sf-yuzifu <yuzifu@yuzifu.top>
2026-01-14 01:05:16 +0800

    style: 修改是否选项

--- src/ui.rs
+++ src/ui.rs
@@ -50,11 +50,19 @@ pub const TAB_CHANGE_EVENT: &str = "tab_change";
 pub const EVENT_NAME_INPUT_EVENT: &str = "event_name_input";
 pub const EVENT_TIME_INPUT_EVENT: &str = "event_time_input";
 pub const ON_INDEX_CHANGE_EVENT: &str = "on_index_change";
+pub const ON_INDEX_YES_EVENT: &str = "on_index_yes";
+pub const ON_INDEX_NO_EVENT: &str = "on_index_no";
 pub const IF_STARING_DAY_CHANGE_EVENT: &str = "if_staring_day_change";
+pub const IF_STARING_DAY_YES_EVENT: &str = "if_staring_day_yes";
+pub const IF_STARING_DAY_NO_EVENT: &str = "if_staring_day_no";
 pub const MODIFY_EVENT_NAME_INPUT_EVENT: &str = "modify_event_name_input";
 pub const MODIFY_EVENT_TIME_INPUT_EVENT: &str = "modify_event_time_input";
 pub const MODIFY_ON_INDEX_CHANGE_EVENT: &str = "modify_on_index_change";
+pub const MODIFY_ON_INDEX_YES_EVENT: &str = "modify_on_index_yes";
+pub const MODIFY_ON_INDEX_NO_EVENT: &str = "modify_on_index_no";
 pub const MODIFY_IF_STARING_DAY_CHANGE_EVENT: &str = "modify_if_staring_day_change";
+pub const MODIFY_IF_STARING_DAY_YES_EVENT: &str = "modify_if_staring_day_yes";
+pub const MODIFY_IF_STARING_DAY_NO_EVENT: &str = "modify_if_staring_day_no";
 pub const ADD_EVENT_BUTTON_EVENT: &str = "add_event_button";
 pub const GET_EVENTS_BUTTON_EVENT: &str = "get_events_button";
 pub const CHANGE_EVENT_BUTTON_EVENT: &str = "change_event_button";
@@ -201,6 +209,110 @@ fn handle_button_click(event: &str) {
                 psys_host::ui::render(&root_id, ui);
             }
         },
+        ON_INDEX_YES_EVENT => {
+            // 设置是否显示在主页为是
+            let root_id: Option<String>;
+            {
+                let mut state = ui_state().write().unwrap_or_else(|poisoned| poisoned.into_inner());
+                state.event_data.on_index = true;
+                root_id = state.root_element_id.clone();
+            }
+            if let Some(root_id) = root_id {
+                let ui = build_main_ui();
+                psys_host::ui::render(&root_id, ui);
+            }
+        },
+        ON_INDEX_NO_EVENT => {
+            // 设置是否显示在主页为否
+            let root_id: Option<String>;
+            {
+                let mut state = ui_state().write().unwrap_or_else(|poisoned| poisoned.into_inner());
+                state.event_data.on_index = false;
+                root_id = state.root_element_id.clone();
+            }
+            if let Some(root_id) = root_id {
+                let ui = build_main_ui();
+                psys_host::ui::render(&root_id, ui);
+            }
+        },
+        IF_STARING_DAY_YES_EVENT => {
+            // 设置是否计入起始日为是
+            let root_id: Option<String>;
+            {
+                let mut state = ui_state().write().unwrap_or_else(|poisoned| poisoned.into_inner());
+                state.event_data.if_staring_day = true;
+                root_id = state.root_element_id.clone();
+            }
+            if let Some(root_id) = root_id {
+                let ui = build_main_ui();
+                psys_host::ui::render(&root_id, ui);
+            }
+        },
+        IF_STARING_DAY_NO_EVENT => {
+            // 设置是否计入起始日为否
+            let root_id: Option<String>;
+            {
+                let mut state = ui_state().write().unwrap_or_else(|poisoned| poisoned.into_inner());
+                state.event_data.if_staring_day = false;
+                root_id = state.root_element_id.clone();
+            }
+            if let Some(root_id) = root_id {
+                let ui = build_main_ui();
+                psys_host::ui::render(&root_id, ui);
+            }
+        },
+        MODIFY_ON_INDEX_YES_EVENT => {
+            // 设置修改事件的是否显示在主页为是
+            let root_id: Option<String>;
+            {
+                let mut state = ui_state().write().unwrap_or_else(|poisoned| poisoned.into_inner());
+                state.modify_event_data.on_index = true;
+                root_id = state.root_element_id.clone();
+            }
+            if let Some(root_id) = root_id {
+                let ui = build_main_ui();
+                psys_host::ui::render(&root_id, ui);
+            }
+        },
+        MODIFY_ON_INDEX_NO_EVENT => {
+            // 设置修改事件的是否显示在主页为否
+            let root_id: Option<String>;
+            {
+                let mut state = ui_state().write().unwrap_or_else(|poisoned| poisoned.into_inner());
+                state.modify_event_data.on_index = false;
+                root_id = state.root_element_id.clone();
+            }
+            if let Some(root_id) = root_id {
+                let ui = build_main_ui();
+                psys_host::ui::render(&root_id, ui);
+            }
+        },
+        MODIFY_IF_STARING_DAY_YES_EVENT => {
+            // 设置修改事件的是否计入起始日为是
+            let root_id: Option<String>;
+            {
+                let mut state = ui_state().write().unwrap_or_else(|poisoned| poisoned.into_inner());
+                state.modify_event_data.if_staring_day = true;
+                root_id = state.root_element_id.clone();
+            }
+            if let Some(root_id) = root_id {
+                let ui = build_main_ui();
+                psys_host::ui::render(&root_id, ui);
+            }
+        },
+        MODIFY_IF_STARING_DAY_NO_EVENT => {
+            // 设置修改事件的是否计入起始日为否
+            let root_id: Option<String>;
+            {
+                let mut state = ui_state().write().unwrap_or_else(|poisoned| poisoned.into_inner());
+                state.modify_event_data.if_staring_day = false;
+                root_id = state.root_element_id.clone();
+            }
+            if let Some(root_id) = root_id {
+                let ui = build_main_ui();
+                psys_host::ui::render(&root_id, ui);
+            }
+        },
         _ => {},
     }
 }
@@ -253,26 +365,74 @@ fn build_add_event_ui(state: &UiState) -> ui::Element {
         .width_full();
     
     // 是否显示在主页
-    let on_index_label = ui::Element::new(ui::ElementType::P, Some("是否显示在主页"))
-        .size(16)
-        .margin_bottom(8);
-    let on_index_dropdown = ui::Element::new(ui::ElementType::Button, Some("是"))
-        .on(ui::Event::Click, ON_INDEX_CHANGE_EVENT)
-        .radius(8)
-        .padding(12)
-        .bg("#2A2A2D")
+    let on_index_container = ui::Element::new(ui::ElementType::Div, None)
+        .relative()
+        .margin_bottom(8)
+        .height(40)
         .width_full();
+    let on_index_label = ui::Element::new(ui::ElementType::P, Some("是否显示在主页"))
+        .absolute()
+        .size(16)
+        .left(0)
+        .top(10);
+    let on_index_buttons = ui::Element::new(ui::ElementType::Div, None)
+        .absolute()
+        .flex()
+        .flex_direction(ui::FlexDirection::Row)
+        .right(0)
+        .top(5);
+    let on_index_yes = ui::Element::new(ui::ElementType::Button, Some("是"))
+        .on(ui::Event::Click, ON_INDEX_YES_EVENT)
+        .radius(8)
+        .padding_top(8)
+        .padding_right(25)
+        .padding_bottom(8)
+        .padding_left(25)
+        .bg(if state.event_data.on_index { "#424243" } else { "#2A2A2D" })
+        .margin_right(5);
+    let on_index_no = ui::Element::new(ui::ElementType::Button, Some("否"))
+        .on(ui::Event::Click, ON_INDEX_NO_EVENT)
+        .radius(8)
+        .padding_top(8)
+        .padding_right(25)
+        .padding_bottom(8)
+        .padding_left(25)
+        .bg(if !state.event_data.on_index { "#424243" } else { "#2A2A2D" });
     
     // 是否计入起始日
-    let if_staring_day_label = ui::Element::new(ui::ElementType::P, Some("是否计入起始日"))
-        .size(16)
-        .margin_bottom(8);
-    let if_staring_day_dropdown = ui::Element::new(ui::ElementType::Button, Some("否"))
-        .on(ui::Event::Click, IF_STARING_DAY_CHANGE_EVENT)
-        .radius(8)
-        .padding(12)
-        .bg("#2A2A2D")
+    let if_staring_day_container = ui::Element::new(ui::ElementType::Div, None)
+        .relative()
+        .margin_bottom(8)
+        .height(40)
         .width_full();
+    let if_staring_day_label = ui::Element::new(ui::ElementType::P, Some("是否计入起始日"))
+        .absolute()
+        .size(16)
+        .left(0)
+        .top(10);
+    let if_staring_day_buttons = ui::Element::new(ui::ElementType::Div, None)
+        .absolute()
+        .flex()
+        .flex_direction(ui::FlexDirection::Row)
+        .right(0)
+        .top(5);
+    let if_staring_day_yes = ui::Element::new(ui::ElementType::Button, Some("是"))
+        .on(ui::Event::Click, IF_STARING_DAY_YES_EVENT)
+        .radius(8)
+        .padding_top(8)
+        .padding_right(25)
+        .padding_bottom(8)
+        .padding_left(25)
+        .bg(if state.event_data.if_staring_day { "#424243" } else { "#2A2A2D" })
+        .margin_right(5);
+    let if_staring_day_no = ui::Element::new(ui::ElementType::Button, Some("否"))
+        .on(ui::Event::Click, IF_STARING_DAY_NO_EVENT)
+        .radius(8)
+        .padding_top(8)
+        .padding_right(25)
+        .padding_bottom(8)
+        .padding_left(25)
+        .bg(if !state.event_data.if_staring_day { "#424243" } else { "#2A2A2D" });
     
     // 添加事件按钮
     let add_event_button = ui::Element::new(ui::ElementType::Button, Some("发送"))
@@ -287,8 +447,8 @@ fn build_add_event_ui(state: &UiState) -> ui::Element {
     container
         .child(ui::Element::new(ui::ElementType::Div, None).child(event_name_label).child(event_name_input))
         .child(ui::Element::new(ui::ElementType::Div, None).child(event_time_label).child(event_time_input))
-        .child(ui::Element::new(ui::ElementType::Div, None).child(on_index_label).child(on_index_dropdown))
-        .child(ui::Element::new(ui::ElementType::Div, None).child(if_staring_day_label).child(if_staring_day_dropdown))
+        .child(on_index_container.child(on_index_label).child(on_index_buttons.child(on_index_yes).child(on_index_no)))
+        .child(if_staring_day_container.child(if_staring_day_label).child(if_staring_day_buttons.child(if_staring_day_yes).child(if_staring_day_no)))
         .child(add_event_button)
 }
 
@@ -332,27 +492,74 @@ fn build_modify_event_ui(state: &UiState) -> ui::Element {
         .width_full();
     
     // 是否显示在主页
-    let on_index_label = ui::Element::new(ui::ElementType::P, Some("是否显示在主页"))
-        .size(16)
-        .margin_bottom(8);
-    let on_index_dropdown = ui::Element::new(ui::ElementType::Button, Some(if state.modify_event_data.on_index { "是" } else { "否" }))
-        .on(ui::Event::Click, MODIFY_ON_INDEX_CHANGE_EVENT)
-        .radius(8)
-        .padding(12)
-        .bg("#2A2A2D")
+    let on_index_container = ui::Element::new(ui::ElementType::Div, None)
+        .relative()
+        .margin_bottom(8)
+        .height(40)
         .width_full();
+    let on_index_label = ui::Element::new(ui::ElementType::P, Some("是否显示在主页"))
+        .absolute()
+        .size(16)
+        .left(0)
+        .top(10);
+    let on_index_buttons = ui::Element::new(ui::ElementType::Div, None)
+        .absolute()
+        .flex()
+        .flex_direction(ui::FlexDirection::Row)
+        .right(0)
+        .top(5);
+    let on_index_yes = ui::Element::new(ui::ElementType::Button, Some("是"))
+        .on(ui::Event::Click, MODIFY_ON_INDEX_YES_EVENT)
+        .radius(8)
+        .padding_top(8)
+        .padding_right(25)
+        .padding_bottom(8)
+        .padding_left(25)
+        .bg(if state.modify_event_data.on_index { "#424243" } else { "#2A2A2D" })
+        .margin_right(5);
+    let on_index_no = ui::Element::new(ui::ElementType::Button, Some("否"))
+        .on(ui::Event::Click, MODIFY_ON_INDEX_NO_EVENT)
+        .radius(8)
+        .padding_top(8)
+        .padding_right(25)
+        .padding_bottom(8)
+        .padding_left(25)
+        .bg(if !state.modify_event_data.on_index { "#424243" } else { "#2A2A2D" });
     
     // 是否计入起始日
-    let if_staring_day_label = ui::Element::new(ui::ElementType::P, Some("是否计入起始日"))
-        .size(16)
-        .margin_bottom(8);
-    let if_staring_day_dropdown = ui::Element::new(ui::ElementType::Button, Some(if state.modify_event_data.if_staring_day { "是" } else { "否" }))
-        .on(ui::Event::Click, MODIFY_IF_STARING_DAY_CHANGE_EVENT)
-        
-        .radius(8)
-        .padding(12)
-        .bg("#2A2A2D")
+    let if_staring_day_container = ui::Element::new(ui::ElementType::Div, None)
+        .relative()
+        .margin_bottom(8)
+        .height(40)
         .width_full();
+    let if_staring_day_label = ui::Element::new(ui::ElementType::P, Some("是否计入起始日"))
+        .absolute()
+        .size(16)
+        .left(0)
+        .top(10);
+    let if_staring_day_buttons = ui::Element::new(ui::ElementType::Div, None)
+        .absolute()
+        .flex()
+        .flex_direction(ui::FlexDirection::Row)
+        .right(0)
+        .top(5);
+    let if_staring_day_yes = ui::Element::new(ui::ElementType::Button, Some("是"))
+        .on(ui::Event::Click, MODIFY_IF_STARING_DAY_YES_EVENT)
+        .radius(8)
+        .padding_top(8)
+        .padding_right(25)
+        .padding_bottom(8)
+        .padding_left(25)
+        .bg(if state.modify_event_data.if_staring_day { "#424243" } else { "#2A2A2D" })
+        .margin_right(5);
+    let if_staring_day_no = ui::Element::new(ui::ElementType::Button, Some("否"))
+        .on(ui::Event::Click, MODIFY_IF_STARING_DAY_NO_EVENT)
+        .radius(8)
+        .padding_top(8)
+        .padding_right(25)
+        .padding_bottom(8)
+        .padding_left(25)
+        .bg(if !state.modify_event_data.if_staring_day { "#424243" } else { "#2A2A2D" });
     
     // 按钮组容器
     let button_group = ui::Element::new(ui::ElementType::Div, None)
@@ -382,8 +589,8 @@ fn build_modify_event_ui(state: &UiState) -> ui::Element {
         .child(ui::Element::new(ui::ElementType::Div, None).child(select_event_label).child(select_event_dropdown))
         .child(ui::Element::new(ui::ElementType::Div, None).child(event_name_label).child(event_name_input))
         .child(ui::Element::new(ui::ElementType::Div, None).child(event_time_label).child(event_time_input))
-        .child(ui::Element::new(ui::ElementType::Div, None).child(on_index_label).child(on_index_dropdown))
-        .child(ui::Element::new(ui::ElementType::Div, None).child(if_staring_day_label).child(if_staring_day_dropdown))
+        .child(on_index_container.child(on_index_label).child(on_index_buttons.child(on_index_yes).child(on_index_no)))
+        .child(if_staring_day_container.child(if_staring_day_label).child(if_staring_day_buttons.child(if_staring_day_yes).child(if_staring_day_no)))
         .child(button_group.child(get_events_button).child(sync_button))
 }
 
